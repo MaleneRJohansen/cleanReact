8559bdfdd2b92ddf68291e1f041149a066d56ce1
MaleneRJohansen <malene9@hotmail.no>
2024-03-04 14:22:07 +0100

    Funnet frem ting fra forelesning

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import Categories from './components/Categories'
+import Layout from './components/Layout'
+import Category from './components/Category'
+import CategoriesIndex from './components/CategoriesIndex'
+import { posts } from './assets/posts'
+import PostPage from './components/PostPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  console.log(posts)
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="categories/*" element={<Categories />}>
+          <Route index element={<CategoriesIndex />}/>
+          <Route path=':slug' element={<Category />}/>
+          <Route path=':slug/:postid' element={<PostPage />} />
+        </Route>
+      </Routes>
+    </Layout>
   )
 }
 
